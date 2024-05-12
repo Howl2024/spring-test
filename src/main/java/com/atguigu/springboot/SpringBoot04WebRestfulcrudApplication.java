342572b96ce0b13cafaa8228695a833fb090c442
Calcifer <Calcifer@163.com>
2024-05-12 17:50:16 +0800

    hot-fix first commit

--- src/main/java/com/atguigu/springboot/SpringBoot04WebRestfulcrudApplication.java
+++ src/main/java/com/atguigu/springboot/SpringBoot04WebRestfulcrudApplication.java
@@ -13,6 +13,7 @@ public class SpringBoot04WebRestfulcrudApplication {
 	public void hello(){
 		System.out.println("hello Git from ideaIDE");
         System.out.println("hello2");
-    }
+		System.out.println("hello3 hot-fix");
+	}
 
 }
